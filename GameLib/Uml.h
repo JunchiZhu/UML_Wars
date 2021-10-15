97ae5dfd26d33d7c9e6792c2a82378f37c04429a
ybw0014 <i@guizhanss.com>
2021-10-16 00:40:56 +0800

    Basic structure for uml

---
+++ GameLib/Uml.h
@@ -0,0 +1,24 @@
+/**
+ * @file Uml.h
+ *
+ * @author ybw0014
+ *
+ * The item class that represents an UML in-game
+ */
+
+#ifndef UML_WARS_UML_H
+#define UML_WARS_UML_H
+
+#include "Item.h"
+
+/**
+ * The Uml item class in-game
+ */
+class Uml : public Item {
+private:
+
+public:
+
+};
+
+#endif //UML_WARS_UML_H
---
+++ GameLib/UmlLoader.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file UmlLoader.cpp
+ *
+ * @author ybw0014
+ */
+
+#include "pch.h"
+#include "UmlLoader.h"
+
+///
+const std::wstring DataFilename = L"data/uml.xml";
+
+/**
+ * Loads uml data from xml
+ */
+void UmlLoader::Load()
+{
+
+}
