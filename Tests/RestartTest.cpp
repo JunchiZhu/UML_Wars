0cfa0dab10020ee0d487c539b6d1d5fe93e10445
Matthew Baxter <baxterm8@msu.edu>
2021-10-22 05:39:29 -0400

    Finished Restart test

--- Tests/RestartTest.cpp
+++ Tests/RestartTest.cpp
@@ -13,7 +13,7 @@
 
 
 TEST(RestartTest, Construct) {
-    // Create an item to test
+    // Create a game to test
     Game game;
 
     game.IncrementCorrect();
@@ -32,7 +32,21 @@ TEST(RestartTest, Construct) {
 
     ASSERT_TRUE(test[1] == 0);
 
-    ASSERT_TRUE(test[2] == 0);
+    ASSERT_TRUE(test[2] == 0);\
+
+
+    Uml goodUml1(&game, L"ClassNameOnly", {}, {}, L"");
+    Uml goodUml2(&game, L"OneAttribute", {L"age: int"}, {}, L"");
+    Uml goodUml3(&game, L"AndOneOperation", {L"hit: bool"}, {L"Shoot()"},L"");
+    Uml badUml1(&game, L"badClass", {}, {}, L"Should capitalize");
+    Uml badUml2(&game, L"badAttribute", {L"Age: int"}, {}, L"Don't capitalize");
+
+
+
+    game.Reset();
+
+
+    ASSERT_TRUE(game.IsEmpty());
 
 
 
